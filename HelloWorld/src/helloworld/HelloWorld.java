0b19470f6147b47c222c2d6fcffab978f3b5325b
Scott Cox <scottcox@san.rr.com>
2015-09-25 16:27:13 -0700

    Changed on github

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("woot\n");
+        System.out.print("This is an edit\n");
     }
     
 }
