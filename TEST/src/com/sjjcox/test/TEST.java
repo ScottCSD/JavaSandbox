9e1990a51435d88a51913f5d1b3da89567d31486
Scott Cox <scott@sjjcox.com>
2015-09-28 13:41:09 -0700

    Package name
    Testing package name process

--- TEST/src/com/sjjcox/test/TEST.java
+++ TEST/src/com/sjjcox/test/TEST.java
@@ -3,17 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package helloworldapp;
-
+package com.sjjcox.test;
 /**
- * com.sjjcox.package
+ *
  * @author scox
  */
-public class HelloWorldApp {
+public class TEST {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        System.out.println("package name test");
     }
 }
